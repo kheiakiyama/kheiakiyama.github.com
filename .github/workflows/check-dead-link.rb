6e5ae44f3d4c8c387961885ac288b03782fe0bcc
Kohei Akiyama <kheiakiyama@gmail.com>
2021-06-03 10:57:21 +0900

    remove jekyll content

--- .github/workflows/check-dead-link.rb
+++
@@ -1,9 +0,0 @@
-require "html-proofer"
-
-HTMLProofer.check_directory("./_site", opts = {
-:check_html => true,
-:check_img_http => true,
-:report_invalid_tags => true,
-:only_4xx => true,
-:url_ignore => [/twitter.com/],
-}).run
